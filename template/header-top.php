f9478b6cdb5d9b27a27eb795a28f016e3924fbfa
pengwei1024 <pengwei1024@gmail.com>
2015-09-05 02:12:45 +0800

    修改文字tag样式

--- template/header-top.php
+++ template/header-top.php
@@ -6,8 +6,8 @@
     </div>
     <div class="float-right">
         <ul style="list-style-type: none">
-            <li><a href="javascript:void(0)">悦读</a></li>
-            <li><a href="<?php bloginfo('url'); ?>/spewing">吐槽</a></li>
+            <li><a href="https://github.com/pengwei1024" target="_blank">Github</a></li>
+            <li><a href="http://weibo.com/2631836861" target="_blank">微博</a></li>
             <li><a href="<?php bloginfo('url'); ?>?feed=rss2" target="_blank">RSS</a></li>
             <li><a href="<?php bloginfo('url'); ?>/about">关于</a></li>
             <li><a href="<?php bloginfo('url'); ?>/wp-admin"><?php echo is_user_logged_in()?"管理":"登录" ?></a></li>
